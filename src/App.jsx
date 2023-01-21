07ab61f50f11f09a8f33cc4a4140be916c7b5d3f
space-brain <106451385+space-brain@users.noreply.github.com>
2023-01-21 23:32:15 +0100

    started project

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,48 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+const sudokuGrid=[
+  [8,0,0,0,0,0,0,0,0],
+  [0,0,0,4,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0],
+  [0,0,2,0,0,0,1,0,0],
+  [0,0,0,0,6,0,0,0,0],
+  [0,9,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0],
+  [0,1,0,0,0,6,0,0,0],
+  [0,0,0,0,0,0,0,0,0],
+]
+
 function App() {
   const [count, setCount] = useState(0)
 
+  const sudokuGridElements=sudokuGrid.map( row => {
+    console.log(row)
+    const sudokuGridColumn=row.map(element => {
+      console.log(element)
+      return (
+        <div>{element===0 ? "":element}</div>
+      )
+    })
+    return (
+      <>
+        {sudokuGridColumn}
+      </>
+    )
+  })
+
+  function randomizeSudokGrid(){
+    
+  }
+
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h1>Sudoku!</h1>
+      <div className="sudoku-container">
+
+      <div className="sudoku-grid">{sudokuGridElements}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </div>
   )
 }
