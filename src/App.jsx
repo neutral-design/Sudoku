adf3f0869a0a9842df992c4589a99dcecb4a0b2b
space-brain <106451385+space-brain@users.noreply.github.com>
2023-01-22 22:55:00 +0100

    started some basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import Sudoku from './Sudoku'
 import './App.css'
 
-const sudokuGrid=[
-  [8,0,0,0,0,0,0,0,0],
-  [0,0,0,4,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0],
-  [0,0,2,0,0,0,1,0,0],
-  [0,0,0,0,6,0,0,0,0],
-  [0,9,0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0,0,0],
-  [0,1,0,0,0,6,0,0,0],
-  [0,0,0,0,0,0,0,0,0],
-]
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
-  const sudokuGridElements=sudokuGrid.map( row => {
-    console.log(row)
-    const sudokuGridColumn=row.map(element => {
-      console.log(element)
-      return (
-        <div>{element===0 ? "":element}</div>
-      )
-    })
-    return (
-      <>
-        {sudokuGridColumn}
-      </>
-    )
-  })
-
-  function randomizeSudokGrid(){
-    
-  }
 
   return (
     <div className="App">
       <h1>Sudoku!</h1>
       <div className="sudoku-container">
 
-      <div className="sudoku-grid">{sudokuGridElements}</div>
+      <Sudoku />
       </div>
       
     </div>
