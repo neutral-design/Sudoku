a4abed1425501a0e01bdcdca5fbf0cdb578af4ee
space-brain <106451385+space-brain@users.noreply.github.com>
2023-01-29 22:43:45 +0100

    Update SudokuGrid.jsx

--- src/SudokuGrid.jsx
+++ src/SudokuGrid.jsx
@@ -127,7 +127,7 @@ function SudokuGrid(props){
     useEffect(()=>{
       // Check entire grid for illegal cells
       const illegalArray=[...new Set(grid.map((item,index)=> {
-        return checkCell(index)
+        return checkCell(grid, index)
       }).flat())]
       
       // setIllegalCells([...new Set(illegalArray)])
@@ -138,8 +138,8 @@ function SudokuGrid(props){
 
 
     // Check if cell-value is legal
-    function checkCell(cellIndex){
-      if(grid[cellIndex]===0){
+    function checkCell(gridToCheck, cellIndex){
+      if(gridToCheck[cellIndex]===0){
         return null
       }
       const returnArray=[]
@@ -150,7 +150,7 @@ function SudokuGrid(props){
       // check row
       for(let i = row*9;i < row*9+9; i++){
         
-        if(grid[cellIndex]===grid[i] && cellIndex!==i){
+        if(gridToCheck[cellIndex]===gridToCheck[i] && cellIndex!==i){
 
           returnArray.push(i)
         }
@@ -160,7 +160,7 @@ function SudokuGrid(props){
 
       for(let i = col; i <= (72+col); i+=9){
         
-        if(grid[cellIndex]===grid[i] && cellIndex!==i){
+        if(gridToCheck[cellIndex]===gridToCheck[i] && cellIndex!==i){
 
           returnArray.push(i)
         }
@@ -174,7 +174,7 @@ function SudokuGrid(props){
         for(let x = 0; x < 3; x++){
           const subGridIndex=subGridY*27+subGridX*3+y*9+x
           
-          if(grid[subGridIndex]===grid[cellIndex] && cellIndex!==subGridIndex){
+          if(gridToCheck[subGridIndex]===gridToCheck[cellIndex] && cellIndex!==subGridIndex){
             
             returnArray.push(subGridIndex)
           }
@@ -189,6 +189,106 @@ function SudokuGrid(props){
       
       
     }
+
+    // Check whether it will be legal
+    // to assign num to the
+    // given row, col
+    function isSafe(grid, row, col, num)
+    {
+        
+        // Check if we find the same num
+        // in the similar row , we
+        // return false
+        for(let x = 0; x <= 8; x++)
+            if (grid[row][x] == num)
+                return false;
+    
+        // Check if we find the same num
+        // in the similar column ,
+        // we return false
+        for(let x = 0; x <= 8; x++)
+            if (grid[x][col] == num)
+                return false;
+    
+        // Check if we find the same num
+        // in the particular 3*3
+        // matrix, we return false
+        let startRow = row - row % 3,
+            startCol = col - col % 3;
+            
+        for(let i = 0; i < 3; i++)
+            for(let j = 0; j < 3; j++)
+                if (grid[i + startRow][j + startCol] == num)
+                    return false;
+    
+        return true;
+    }
+
+    let N = 9;
+ 
+    /* Takes a partially filled-in grid and attempts
+        to assign values to all unassigned locations in
+        such a way to meet the requirements for
+        Sudoku solution (non-duplication across rows,
+        columns, and boxes) */
+    function solveSudoku(grid, row, col)
+    {
+        
+        /* If we have reached the 8th
+          row and 9th column (0
+          indexed matrix) ,
+          we are returning true to avoid further
+          backtracking       */
+        if (row == N - 1 && col == N)
+            return true;
+    
+        // Check if column value  becomes 9 ,
+        // we move to next row
+        // and column start from 0
+        if (col == N)
+        {
+            row++;
+            col = 0;
+        }
+    
+        // Check if the current position
+        // of the grid already
+        // contains value >0, we iterate
+        // for next column
+        if (grid[row][col] != 0)
+            return solveSudoku(grid, row, col + 1);
+    
+        for(let num = 1; num < 10; num++)
+        {
+            
+            // Check if it is safe to place
+            // the num (1-9)  in the given
+            // row ,col ->we move to next column
+            if (isSafe(grid, row, col, num))
+            {
+                
+                /*  assigning the num in the current
+                (row,col)  position of the grid and
+                assuming our assigned num in the position
+                is correct */
+                grid[row][col] = num;
+    
+                // Checking for next
+                // possibility with next column
+                if (solveSudoku(grid, row, col + 1))
+                    return true;
+            }
+            
+            /* removing the assigned num , since our
+              assumption was wrong , and we go for next
+              assumption with diff num value   */
+            grid[row][col] = 0;
+        }
+        return false;
+    }
+
+
+
 
     const sudokuGridElements=grid.map( (cell, cellIndex) => {
         const classes=(selectedCell===cellIndex ? "selected ":"")  + (illegalCells.includes(cellIndex) ? "illegal ":"")
