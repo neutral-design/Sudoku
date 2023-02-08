15c029ce93972ddd4dea18172b4ba94902d512dd
space-brain <106451385+space-brain@users.noreply.github.com>
2023-02-08 21:29:01 +0100

    changes

--- src/SudokuGrid.jsx
+++ src/SudokuGrid.jsx
@@ -431,7 +431,8 @@ function SudokuGrid(props){
                 >Solve</button>
               </div>
               <InputGrid setCell={setCell}/>
-        </div>
+              
+          </div>
               
               
               
